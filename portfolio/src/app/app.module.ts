e941b4dbd6e20bd6ffc52340c005e60f40d75743
Michael Lin <michael.jlin7@gmail.com>
2021-10-03 18:25:41 -0400

    Added a footer, a home page and smooth section scrolling

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     HomeComponent,
     ProjectsComponent,
     WorkExperienceComponent,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
